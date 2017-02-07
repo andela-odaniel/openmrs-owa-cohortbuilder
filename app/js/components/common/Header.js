842665730866de81d880f6995122d2ab3d953775
Fortune Ekeruo <legalmody@gmail.com>
2017-02-07 12:41:29 +0100

    Add a dropdown

--- app/js/components/common/Header.js
+++ app/js/components/common/Header.js
@@ -11,8 +11,26 @@ const Header = () => {
       </div>
 
       <ul className="navbar-right nav-header">
+        <li role="presentation" activeClassName="active" className="dropdown">
+          <a className="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+            <span className="glyphicon glyphicon-user"></span> User <span className="caret"></span>
+          </a>
+          <ul className="dropdown-menu">
+            <li><a href="#">My Account</a></li>
+          </ul>
+        </li>
+        <li role="presentation" activeClassName="active" className="dropdown">
+          <a className="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+            <span className="glyphicon glyphicon glyphicon-map-marker"></span> Impatient ward <span className="caret"></span>
+          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <a href="#">Impatient ward</a>
+            </li>
+          </ul>
+        </li>
         <li role="presentation" className="active">
-        <a href="#">Logout <span className="glyphicon glyphicon-log-out"></span></a>
+          <a href="#">Logout <span className="glyphicon glyphicon-log-out"></span></a>
         </li>
       </ul>
     </header>
